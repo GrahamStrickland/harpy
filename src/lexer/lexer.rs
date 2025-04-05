c6f5155dad2387ab4b7d9d1a78ba7ade6c060d0e
Graham Strickland <grahamhstrickland@gmail.com>
2025-04-05 18:02:36 +0200

    Add more to next() implementation for Lexer

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -34,13 +34,28 @@ impl Iterator for Lexer {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
+        for c in self.text.chars() {
+            if self.punctuators.contains_key(&c) {
+                return Some(Token::new(
+                    *self.punctuators.get(&c).unwrap(),
+                    String::from(c),
+                ));
+            } else if c.is_alphabetic() {
+                let name = String::from(c);
+
+                return Some(Token {
+                    token_type: TokenType::Name,
+                    text: name,
+                });
+            } // else ignore all other characters (whitespace, etc.)
+        }
+
         // Once we've reached the end of the string, just return EOF tokens. We'll
         // just keeping returning them as many times as we're asked so that the
         // parser's lookahead doesn't have to worry about running out of tokens.
-        let token = Token {
+        Some(Token {
             token_type: TokenType::Eof,
             text: String::from(""),
-        };
-        Some(token)
+        })
     }
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -2,6 +2,12 @@ use crate::lexer::token_type::TokenType;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Token {
-    pub(crate) token_type: TokenType,
-    pub(crate) text: String,
+    token_type: TokenType,
+    pub text: String,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, text: String) -> Token {
+        Self { token_type, text }
+    }
 }
